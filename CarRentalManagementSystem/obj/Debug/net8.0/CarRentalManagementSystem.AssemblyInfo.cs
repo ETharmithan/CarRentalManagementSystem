53b2c3c0d16f2ada25420eb388de055b13080f74
ETharmithan <tharmithan5@gmail.com>
2025-07-24 08:27:33 +0530

    New 24.07.2025

--- CarRentalManagementSystem/obj/Debug/net8.0/CarRentalManagementSystem.AssemblyInfo.cs
+++ CarRentalManagementSystem/obj/Debug/net8.0/CarRentalManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarRentalManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a228771b09e4011daf2a0aa52a64d7d4e9a3c52")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarRentalManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarRentalManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
